8c17601a07f1822d84e467a4d681093ad7e94a04
Mark Woodhall <mark.woodhall@gmail.com>
2024-09-28 22:59:09 +0100

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,7 +7,7 @@
  '(custom-safe-themes
    '("4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" default))
  '(package-selected-packages
-   '(volatile-highlights nerd-icons-dired catppuccin-theme ef-themes evil-smartparens exec-path-from-shell eldoc-box smex doom-themes yasnippet yaml ws-butler which-key vterm undo-tree toc-org terraform-mode sudo-edit smartparens rainbow-delimiters projectile org-bullets magit lua-mode lsp-ui lsp-treemacs lsp-ivy kotlin-mode kind-icon ivy-xref ivy-rich highlight-indent-guides git-gutter general flycheck-kotlin fennel-mode feature-mode evil-surround evil-goggles evil-collection doom-modeline dockerfile-mode docker dashboard counsel company cider all-the-icons)))
+   '(evil-cleverparens nerd-icons-dired catppuccin-theme ef-themes evil-smartparens exec-path-from-shell eldoc-box smex doom-themes yasnippet yaml ws-butler which-key vterm undo-tree toc-org terraform-mode sudo-edit smartparens rainbow-delimiters projectile org-bullets magit lua-mode lsp-ui lsp-treemacs lsp-ivy kotlin-mode kind-icon ivy-xref ivy-rich highlight-indent-guides git-gutter general flycheck-kotlin fennel-mode feature-mode evil-surround evil-goggles evil-collection doom-modeline dockerfile-mode docker dashboard counsel company cider all-the-icons)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
