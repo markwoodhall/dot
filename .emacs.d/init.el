6e5d29491984383fb4c1ddebf690da996fdfb3d7
Mark Woodhall <mark.woodhall@gmail.com>
2026-05-05 21:30:14 +0100

    Update sage themes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,7 +5,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("c4df9006b9eb32599d758800a32f3487c2cdf13826084511783b47d419024af2"
+   '("b3ed1a4cf401a667d2ec3d8d62eb2062db0476f8c8990d8be8483deac7431c58"
+     "0e4a97c12619ddf2892b0de051512ecc3c4d896042a4392b052177bfab23a6e4"
+     "c4df9006b9eb32599d758800a32f3487c2cdf13826084511783b47d419024af2"
      "f9d423fcd4581f368b08c720f04d206ee80b37bfb314fa37e279f554b6f415e9"
      "e604027b0160128a9bd633a63cd111227eea017cfbbe1ff5f3f7c83c9fb72bdf"
      "6755a9491a6287586f3c2e57e0fc7411afab62e10a1401fc88147ec06317eafe"
