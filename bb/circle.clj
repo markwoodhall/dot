e31a52a3b1fb0e7108404575e40e99abae97a415
Mark Woodhall <mark.woodhall@gmail.com>
2026-06-25 11:03:21 +0100

    Add circle url to output for easy url jump

--- bb/circle.clj
+++ bb/circle.clj
@@ -8,7 +8,7 @@
 
 (def config 
   (edn/read-string 
-    (slurp (str (System/getenv "HOME") "/.config/circlebb.edn"))))
+   (slurp (str (System/getenv "HOME") "/.config/circlebb.edn"))))
 
 (defonce api "https://circleci.com/api/v2/")
 
@@ -35,42 +35,53 @@
   [vc org project]
   (let [url (str/join "/" [api "project" vc org project "pipeline"])]
     (->
-      (http/get url
-                {:headers {"Circle-Token" (-> config :token)}})
-      :body
-      (json/parse-string true)
-      :items
-      first
-      :id)))
+     (http/get url
+               {:headers {"Circle-Token" (-> config :token)}})
+     :body
+     (json/parse-string true)
+     :items
+     first
+     :id)))
 
-(defn workflow-status [pipeline-id]
+(defn workflow [pipeline-id]
   (let [url (str/join "/" [api "pipeline" pipeline-id "workflow"])]
     (->
-      (http/get url
-                {:headers {"Circle-Token" (-> config :token)}})
-      :body
-      (json/parse-string true)
-      :items
-      first
-      :status)))
+     (http/get url
+               {:headers {"Circle-Token" (-> config :token)}})
+     :body
+     (json/parse-string true)
+     :items
+     first)))
+
+(defn circle-url
+  [vc org project pipeline-number workflow-id]
+  (str " https://app.circleci.com/pipelines/"
+       (case vc "gh" "github/" vc)
+       org "/"
+       project "/"
+       pipeline-number
+       "/workflows/" workflow-id))
 
 (doseq [[vc org project] (-> config :projects)]
-  (let [status (try (-> (latest-pipeline vc org project)
-                        workflow-status)
-                    (catch Exception e
-                      (eprintln (ex-data e))
-                      "failed-to-determine"))
+  (let [{:keys [status id pipeline_number]}
+        (try (-> (latest-pipeline vc org project)
+                 workflow)
+             (catch Exception e
+               (eprintln (ex-data e))
+               "failed-to-determine"))
         output (case status
                  ("running" "on_hold")  
                  (str vc "/" org "/" project " " (colorize yellow status))
                  "success" 
-                 (str vc "/" org "/" project " " (colorize green status))
+                 (str vc "/" org "/" project " " (colorize green status)
+                      (circle-url vc org project pipeline_number id))
                  ("failed" 
                   "error" 
                   "failing" 
-                  "canceled" 
+                  "cancelled"
                   "unauthorized"
                   "failed-to-determine") 
-                 (str vc "/" org "/" project " " (colorize red status) )
+                 (str vc "/" org "/" project " " (colorize red status)
+                      (circle-url vc org project pipeline_number id))
                  (str vc "/" org "/" project " " status))]
     (println output)))
